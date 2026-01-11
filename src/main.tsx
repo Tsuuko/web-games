88d0ef7751d82fd09e3cada96a238d1514e69586
Tsuuko <23350171+Tsuuko@users.noreply.github.com>
2026-01-12 05:37:04 +0900

    ダブルタップ無効化修正

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,18 @@ import './index.css';
 import { App } from './app.tsx';
 
 render(<App />, document.getElementById('app')!);
+
+// iOSのダブルタップ拡大を無効化
+let lastTouchEnd = 0;
+
+document.addEventListener(
+  'touchend',
+  (event: TouchEvent) => {
+    const now = Date.now();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  },
+  false,
+);
